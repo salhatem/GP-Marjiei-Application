681ed35510a34d2564be3f2f7c68d23193d6798d
Shatha Suliman <Shatha Suliman@Shaha-Suliman>
2018-02-16 12:48:22 +0300

    Add WebPage Constructors.

--- src/WebPage.java
+++ src/WebPage.java
@@ -12,4 +12,27 @@ import java.util.Date;
 public class WebPage extends ReferenceDocument {
     private Date AccessDate;
     private String URL;
+    
+      // Constructors
+       public WebPage ()
+    {
+        Title = " "; 
+        Author = " "; 
+        Publisher = " "; 
+        PublishYear = 0;
+        PagesNo = " ";
+        AccessDate = null;
+        URL = " ";
+    }
+       
+    public WebPage (String title, String author, String publisher, int pYear, String pages, Date aDate, String url)
+    {
+        Title = title; 
+        Author = author; 
+        Publisher = publisher; 
+        PublishYear = pYear;
+        PagesNo = pages;
+        AccessDate = aDate;
+        URL = url; 
+    }
 }
