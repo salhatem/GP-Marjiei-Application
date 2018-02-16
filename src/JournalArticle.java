ec689ecfc921fc9501c36a4b981774f0d4f2cdba
Shatha Suliman <Shatha Suliman@Shaha-Suliman>
2018-02-16 12:50:59 +0300

    Add JournalArticle Constructors.

--- src/JournalArticle.java
+++ src/JournalArticle.java
@@ -11,4 +11,27 @@
 public class JournalArticle extends ReferenceDocument {
     private String JournalName;
     private int Volume;
+    
+      // Constructors
+       public JournalArticle ()
+    {
+        Title = " "; 
+        Author = " "; 
+        Publisher = " "; 
+        PublishYear = 0;
+        PagesNo = " ";
+        JournalName = " ";
+        Volume = 0;
+    }
+       
+    public JournalArticle (String title, String author, String publisher, int pYear, String pages, String jName, int volume)
+    {
+        Title = title; 
+        Author = author; 
+        Publisher = publisher; 
+        PublishYear = pYear;
+        PagesNo = pages;
+        JournalName = jName;
+        Volume = volume;
+    }
 }
